c8855bab22caa4709b1e4f5f1e7951aa30271392
Sungmin Hong <sm98310@gmail.com>
2023-05-11 02:41:50 +0900

    [ADD] 로그인정보 컴포넌트
    -  progressbar  & 로그인컴포넌트 반응형 디자인
    - 유효성 검사

---
+++ src/components/signUp/LoginInfo.jsx
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export default function LoginInfo({ loginData, setLoginData }) {
+  const [password, setPassword] = useState('');
+  // 비밀번호 패턴 확인을 위함
+  const [pwError, setpwError] = useState(false);
+  // 비밀번호 일치 확인을 위함
+  const [checkPw, setCheckPw] = useState(false);
+
+  // 이메일 인증확인
+  const [confirmEmail, setConfirmEmail] = useState(false);
+
+  // loginData 설정
+  const handleChange = e => {
+    setLoginData({ ...loginData, [e.target.id]: e.target.value });
+  };
+
+  // 비밀번호 패턴 확인
+  const validPassword = e => {
+    const reg =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&^])[A-Za-z\d@$!%*#?&^]{8,16}$/;
+    if (reg.test(e.target.value)) {
+      setPassword(e.target.value);
+      setpwError(false);
+    } else {
+      setpwError(true);
+    }
+  };
+
+  // 비밀번호 일치 확인
+  const checkPassword = e => {
+    if (e.target.value === password) {
+      setCheckPw(false);
+      e.target.id = 'password';
+      handleChange(e);
+    } else {
+      setCheckPw(true);
+    }
+  };
+
+  // console.log(`${`123456789`}`);
+
+  return (
+    <Container>
+      <Title>로그인 정보</Title>
+      <TextField>
+        <SubTitle>아이디</SubTitle>
+        <Input type="text" id="userId" onChange={handleChange} />
+      </TextField>
+      <TextField>
+        <SubTitle>이름</SubTitle>
+        <Input type="text" id="managerName" onChange={handleChange} />
+      </TextField>
+      <TextField>
+        <SubTitle>연락처</SubTitle>
+        <Input type="text" id="phoneNumber" onChange={handleChange} />
+      </TextField>
+      <TextField>
+        <SubTitle>이메일</SubTitle>
+        <Input type="email" id="email" onChange={handleChange} />
+        <Btn finish={confirmEmail} onClick={e => e.preventDefault()}>
+          {confirmEmail ? '인증완료' : '인증메일발송'}
+        </Btn>
+      </TextField>
+      <TextField>
+        <SubTitle>
+          비밀번호 (영문 대/소문자, 숫자, 특수문자 조합 8~16자리)
+        </SubTitle>
+        <Input type="password" id="password" onChange={validPassword} />
+      </TextField>
+      {pwError && (
+        <Error>8~16자리의 영문 대/소문자, 숫자, 특수문자를 사용하세요.</Error>
+      )}
+      <TextField>
+        <SubTitle>비밀번호 확인</SubTitle>
+        <Input type="password" id="checkPassword" onChange={checkPassword} />
+      </TextField>
+      {checkPw && <Error>비밀번호가 일치하지 않습니다.</Error>}
+    </Container>
+  );
+}
+
+const Container = styled.form`
+  margin-top: 25px;
+`;
+
+const Title = styled.p`
+  font-size: 24px;
+  color: #2b66f6;
+  margin-bottom: 5px;
+  @media (max-width: 768px) {
+    font-size: 20px;
+  }
+`;
+
+const TextField = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 1.3;
+  color: #797979;
+  margin: 15px auto;
+  position: relative;
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const SubTitle = styled.p`
+  margin-bottom: 2px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 30px;
+  outline: none;
+  padding: 5px;
+  border: 0;
+  color: #434343;
+  border-bottom: 1px solid #d0d0d0;
+  ::placeholder {
+    color: #d9d9d9;
+  }
+`;
+
+const Error = styled.p`
+  color: #d30000;
+  margin: 5px 0;
+  font-size: 12px;
+`;
+
+const Btn = styled.button`
+  width: 95px;
+  height: 30px;
+  border: ${props => (props.finish ? '0' : '2px solid #2b66f6')};
+  border-radius: 999px;
+  background-color: ${props => (props.finish ? '#2b66f6' : 'transparent')};
+  color: ${props => (props.finish ? '#fff' : '#2b66f6')};
+  font-size: 12px;
+  font-weight: 600;
+  margin-top: 15px;
+  position: absolute;
+  bottom: 15%;
+  right: 0;
+  transition: 0.3s ease;
+  @media (max-width: 768px) {
+    width: 80px;
+    height: 28px;
+    font-size: 10px;
+  }
+`;
--- src/pages/user/SignUp.jsx
+++ src/pages/user/SignUp.jsx
@@ -1,5 +1,159 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { BsCheckLg } from 'react-icons/bs';
+import LoginInfo from '../../components/signUp/LoginInfo';
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  // 로그인 정보
+  const [loginData, setLoginData] = useState(false);
+
+  return (
+    <Wrapper>
+      <Logo src="./images/logo_origin.png" />
+      <Progress>
+        <CheckCircle>
+          <Icon>
+            <BsCheckLg />
+          </Icon>
+        </CheckCircle>
+        <ProgressLine left={true} finish={loginData.password?.length} />
+        <CircleFill finish={loginData.password?.length}>2</CircleFill>
+        <ProgressLine />
+        <Circle status={true}>3</Circle>
+      </Progress>
+      <FormContainer>
+        <LoginInfo setLoginData={setLoginData} loginData={loginData} />
+
+        <StartBtn>시작하기</StartBtn>
+      </FormContainer>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  max-width: 450px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 50px auto 100px;
+  padding: 0 10px;
+  @media (max-width: 768px) {
+    padding: 0 12px;
+  }
+`;
+
+const Logo = styled.img`
+  display: none;
+  @media (max-width: 768px) {
+    display: block;
+    width: 170px;
+    margin-bottom: 20px;
+  }
+`;
+
+const Progress = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 15px;
+  position: relative;
+  transition: 0.3s ease;
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`;
+
+const ProgressLine = styled.hr`
+  width: 25%;
+  height: 2px;
+  background-color: ${props => (props.finish ? '#2b66f6' : '#B5CDFA')};
+  border: 0;
+  position: absolute;
+  ${props => (props.left ? 'left: 20%' : 'right : 20%')};
+`;
+
+const CheckCircle = styled.div`
+  width: 55px;
+  height: 55px;
+  border: 2px solid #2b66f6;
+  border-radius: 100%;
+  background-color: #fff;
+  color: #2b66f6;
+  position: relative;
+  z-index: 1;
+  transition: 0.3s ease;
+  @media (max-width: 768px) {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+const Icon = styled.div`
+  font-size: 30px;
+  position: absolute;
+  top: 24%;
+  left: 22%;
+  transition: 0.3s ease;
+  @media (max-width: 768px) {
+    font-size: 25px;
+  }
+`;
+
+const CircleFill = styled.div`
+  width: 55px;
+  height: 55px;
+  border-radius: 100%;
+  background-color: ${props => (props.finish ? '#2b66f6' : '#b5cdfa')};
+  color: #fff;
+  font-size: 25px;
+  text-align: center;
+  line-height: 200%;
+  position: relative;
+  z-index: 1;
+  transition: 0.3s ease;
+  @media (max-width: 768px) {
+    width: 50px;
+    height: 50px;
+    line-height: 180%;
+  }
+`;
+
+const Circle = styled.div`
+  width: 55px;
+  height: 55px;
+  border: ${props =>
+    props.finish ? '2px solid #2b66f6' : '2px solid #b5cdfa'};
+  border-radius: 100%;
+  background-color: #fff;
+  color: ${props => (props.finish ? '#2b66f6' : '#b5cdfa')};
+  font-size: 24px;
+  text-align: center;
+  line-height: 200%;
+  position: relative;
+  z-index: 1;
+  transition: 0.3s ease;
+  @media (max-width: 768px) {
+    width: 50px;
+    height: 50px;
+    line-height: 180%;
+  }
+`;
+
+const FormContainer = styled.div`
+  width: 100%;
+`;
+
+const StartBtn = styled.button`
+  width: 100%;
+  height: 52px;
+  margin-top: 20px;
+  font-size: 16px;
+  font-weight: 600;
+  color: #fff;
+  border: 0;
+  border-radius: 999px;
+  background-color: #2b66f6;
+  cursor: pointer;
+`;
