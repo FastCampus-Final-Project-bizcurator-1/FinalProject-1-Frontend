1cf2c194755b8b54fa1ccecbc5d1375ffaee336e
ssryoung <qkrtpfud816@gmail.com>
2023-05-18 20:57:48 +0900

    [ADD] add header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
+import Header from './components/main/Header';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import variables from './styles/variables';
@@ -9,6 +10,7 @@ import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <ThemeProvider theme={{ style: theme, variables }}>
+      <Header />
       <GlobalStyle />
       <Outlet />
       <Footer />
