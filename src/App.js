c8ec5af17e301289bb6ff61a059d68effc60c4f2
J-Plum <bosswjdwo@gmail.com>
2023-05-20 15:05:43 +0900

    [ADD] axios 추가 및 contextApi 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { ThemeProvider } from 'styled-components';
+import Header from './components/main/Header';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import variables from './styles/variables';
 import Footer from './components/main/Footer';
 import { Outlet } from 'react-router-dom';
+import Apiprovider from './context/context';
 
 function App() {
   return (
     <ThemeProvider theme={{ style: theme, variables }}>
-      <GlobalStyle />
-      <Outlet />
-      <Footer />
+      <Apiprovider>
+        <Header />
+        <GlobalStyle />
+        <Outlet />
+        <Footer />
+      </Apiprovider>
     </ThemeProvider>
   );
 }
